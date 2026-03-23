c6c050c7bfda3cbb4bc9da7ad3cc24739d2fdf73
Pc35 <Pc35@CPU35>
2026-03-23 13:27:02 -0600

    Clase restar

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -7,19 +7,19 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese el primer número:");
+        System.out.println("Ingrese el primer numero:");
         int a = sc.nextInt();
 
-        System.out.println("Ingrese el segundo número:");
+        System.out.println("Ingrese el segundo numero:");
         int b = sc.nextInt();
 
-//        Suma suma = new Suma();
-//       
-//        Multiplicacion multiplicacion = new Multiplicacion();
-//
-//        System.out.println("Suma: " + suma.calcular(a, b));
-//       
-//        System.out.println("Multiplicación: " + multiplicacion.calcular(a, b));
+        Suma suma = new Suma();
+       
+        Multiplicacion multiplicacion = new Multiplicacion();
+
+       System.out.println("Suma: " + suma.suma(a, b));
+       
+        System.out.println("Multiplicacion: " + multiplicacion.calcular(a, b));
 
     }
 }
