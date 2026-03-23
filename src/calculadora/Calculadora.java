35755095dd391065eb20f7c1e09ba03f06ebd0f2
Pc35 <Pc35@CPU35>
2026-03-23 12:22:08 -0600

    Subiendo clase principal Calculadora

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,25 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int a = sc.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = sc.nextInt();
+
+//        Suma suma = new Suma();
+//       
+//        Multiplicacion multiplicacion = new Multiplicacion();
+//
+//        System.out.println("Suma: " + suma.calcular(a, b));
+//       
+//        System.out.println("Multiplicación: " + multiplicacion.calcular(a, b));
+
+    }
+}
