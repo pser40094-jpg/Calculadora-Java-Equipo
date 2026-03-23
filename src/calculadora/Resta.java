22053113592966268ae2076265810496c2f68693
Pc35 <Pc35@CPU35>
2026-03-23 13:23:24 -0600

    Clase resta

---
+++ src/calculadora/Resta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Pc35
+ */
+public class Resta {
+    public int calcular(int a, int b) {
+        return a - b; 
+        
+    }
+}
