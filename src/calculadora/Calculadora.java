671ed738fe8ed50a1d7462d5a3efa4ad379b92a9
Pc4-36 <Pc4-36@CPU36>
2026-03-25 15:32:48 -0600

    Clase multiplicacion

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -13,16 +13,16 @@ public class Calculadora {
         System.out.println("Ingrese el segundo numero:");
         int b = sc.nextInt();
 
-        Suma suma = new Suma();
+        //Suma suma = new Suma();
        
         Multiplicacion multiplicacion = new Multiplicacion();
         
-        Resta calcular = new Resta();
-       System.out.println("Suma: " + suma.suma(a, b));
+       // Resta calcular = new Resta();
+      // System.out.println("Suma: " + suma.suma(a, b));
        
         System.out.println("Multiplicacion: " + multiplicacion.calcular(a, b));
         
-        System.out.println("Resta: " + calcular.calcular(a,b));
+        //System.out.println("Resta: " + calcular.calcular(a,b));
 
     }
 }
