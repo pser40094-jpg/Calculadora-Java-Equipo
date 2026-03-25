7661c212b6f89ed8d1898be6f2e7d7398d40fbd2
Pc34 <Pc34@Cpu34>
2026-03-25 15:24:23 -0600

    Cambios

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -5,24 +5,37 @@ import java.util.Scanner;
 public class Calculadora {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+            
+            Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ingrese el primer numero:");
-        int a = sc.nextInt();
 
-        System.out.println("Ingrese el segundo numero:");
-        int b = sc.nextInt();
-
-        Suma suma = new Suma();
-       
-        Multiplicacion multiplicacion = new Multiplicacion();
+            System.out.println("--------------------> CALCULADORA <--------------------");
+            System.out.println();
+            System.out.println("Ingresar solo numeros enteros");
+            System.out.println();
         
-        Resta calcular = new Resta();
-       System.out.println("Suma: " + suma.suma(a, b));
-       
-        System.out.println("Multiplicacion: " + multiplicacion.calcular(a, b));
-        
-        System.out.println("Resta: " + calcular.calcular(a,b));
+            System.out.println("Ingrese el primer numero:");
+            int a = sc.nextInt();
 
+            System.out.println("Ingrese el segundo numero:");
+            int b = sc.nextInt();
+
+            Suma suma = new Suma();
+       
+            Multiplicacion multiplicacion = new Multiplicacion();
+        
+            Resta calcular = new Resta();
+        
+            System.out.println("--------------------> RESULTADOS <---------------------");
+            
+            System.out.println();
+            System.out.println("Resultado de la suma: " + suma.suma(a, b));
+            
+            System.out.println();
+            System.out.println("Resultado de la resta: " + calcular.calcular(a,b));
+            
+            System.out.println();
+            System.out.println("Resultado de la multiplicacion: " + multiplicacion.calcular(a, b));
+        
     }
 }
